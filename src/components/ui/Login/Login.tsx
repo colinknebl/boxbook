daf0eec37342fc64bdac0c50be8caba588665799
Colin Knebl <colinknebl@outlook.com>
2019-10-20 08:49:09 -0400

    remove test data

--- src/components/ui/Login/Login.tsx
+++ src/components/ui/Login/Login.tsx
@@ -51,8 +51,8 @@ export const StyledLogin = styled.section`
 function Login(props: IProps) {
     const ctx = useContext(MainAppContext);
     const [error, setError] = useState(null);
-    const [email, setEmail] = useState('jessica.knebl@gmail.com');
-    const [password, setPassword] = useState('australia54!');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const onClick = async () => {
         if (email.length && password.length) {
             const data = await ctx.login(email, password);
